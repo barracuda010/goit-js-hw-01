1b9e82ba72ac5871e2e6eed850d9d33c9bc47d6f
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-11-06 10:34:08 +0200

    HW 12V2

--- module-12V2/src/js/countries.js
+++ module-12V2/src/js/countries.js
@@ -1,14 +1,17 @@
 import fetchCountries from './fetchCountries';
 import PNotify from 'pnotify/dist/es/PNotify.js';
 import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial.js';
+import debounce from 'lodash.debounce'
+
+
 const input = document.querySelector('.input');
 const ul = document.querySelector('.country');
-input.addEventListener('input', e => {
+input.addEventListener('input',debounce(e => {
   fetchCountries(e.target.value)
     .then(data =>{
       if(data.length <= 11 && data.length !== 1 ){
         PNotify.defaults.icons = "material";
-      PNotify.alert({
+        PNotify.alert({
           title: 'To many matches found.',
           text: 'Please enter a more specific query!',
           addClass: 'custom nonblock',
@@ -25,11 +28,12 @@ input.addEventListener('input', e => {
           acc += `<h2 class="country__title">${item.name}</h2><div class="country__wrapper"><div class=“left__side”>
           <p><span style="font-weight: bold">Capital:</span>${item.capital}</p>
           <p><span style="font-weight: bold">Population:</span>${item.population}</p>
-          <p><span style="font-weight: bold">Languages:</span></p>
+          <p><span style="font-weight: bold">Languages:</span>$</p>
           </div><div class="right__side"><img src="${item.flag}" width="500" height="400" alt="Flag"></div>`;
           return ul.innerHTML = acc;
         }, '')}
         else {return ''}
     })
     .catch(err => console.log(err));
-});
+
+},500));
